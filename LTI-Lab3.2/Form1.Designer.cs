f7abeebf9cc42ca21e8fb4dc007550d43f14c816
lucas <lucas@LAPTOP-DP9AM068>
2022-03-29 13:26:20 +0100

    Add project files.

---
+++ LTI-Lab3.2/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace LTI_Lab3._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tarefa1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(87, 22);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Scoped token";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Location = new System.Drawing.Point(236, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Tarefa2";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 22);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Unscoped token";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private GroupBox groupBox1;
+        private Button button2;
+        private GroupBox groupBox2;
+        private Button button3;
+    }
+}
---
+++ LTI-Lab3.2/Form1.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+namespace LTI_Lab3._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string? unAuthToken { get; private set; } //?????
+        public string? responseString { get; private set; } //?????
+
+        public string? responseString2 { get; private set; } //?????
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            getResponseString();
+
+
+        }
+
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            getScopedToken();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // EXEMPLO DE UM PEDIDO POST DIRIGIDO AO API DO SERVIcO KEYSTONE (IDENTITY)
+
+            var myWebClient = new WebClient();
+            myWebClient.Headers[HttpRequestHeader.ContentType] = "application/json";
+
+
+            // ... UTILIZANDO JSON: CRIAR UMA STRING COM O BODY A SER ENVIADO NO POST
+
+            String jsonToSend = "{\"auth\": {\"identity\": {\"methods\": [\"password\"],\"password\": {\"user\": {\"name\": \"admin\",\"domain\": {\"name\": \"Default\"},\"password\": \"123a\"}}}}}";
+
+
+            // ... PEDIDO POST AO API DO KEYSTONE
+
+             responseString = myWebClient.UploadString("http://192.168.38.128/identity/v3/auth/tokens/", jsonToSend);
+
+
+            // ... RETIRAR DOS HEADERS RECEBIDOS NA RESPOSTA O TOKEN
+
+            WebHeaderCollection myWebHeaderCollection = myWebClient.ResponseHeaders;
+
+            for (int i = 0; i < myWebHeaderCollection.Count; i++)
+            {
+                if (myWebHeaderCollection.GetKey(i) == "X-Subject-Token")
+                {
+                    unAuthToken = myWebHeaderCollection.Get(i); // GUARDAR O UNSCOPED TOKEN (APENAS UTILIZADO PARA AUTENTICAO E CRIAO DE SCOPED TOKENS), NaO PERMITE ACESSO AOS DETALHES DOS SERVIcOS
+                }
+            }
+
+
+            //TAREFA 2
+            var myWebClient2 = new WebClient();
+            myWebClient2.Headers[HttpRequestHeader.ContentType] = "application/json";
+            myWebClient2.Headers.Add("x-auth-token", "gAAAAABiNhTSL9aHqPAxXT4WhnetjtYmkaQ6ub3nCXYL0GgEiS9WVwMboPh_YzgcOvmdhicQiF40xsE3wCNaatc_gS_53yw6pfE44yY-paD9Of08DiOHtwtWbCYEmcmPeE9lYIn5f70B952YGdaIf7Y0kxtHM-pvetmGuLwNeVn6RL8L9Mpk9-s");
+
+      //      responseString2 = myWebClient2.DownloadString("http://192.168.38.128/identity/v3/auth/projects");
+
+        }
+
+        private void getScopedToken()
+        {
+            MessageBox.Show(unAuthToken, "UnAuthToken");
+        }
+        private void getResponseString()
+        {
+            MessageBox.Show(responseString, "Json de resposta");
+        }
+
+        private void getUnscopedToken()
+        {
+            MessageBox.Show(responseString2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            getUnscopedToken();
+        }
+    }
+
+
+ 
+}
+
